b0ae43bebd1ee0fa5109a20c25eda0129e3ade90
Mira Chen <mirasc25@gmail.com>
2016-03-30 00:03:44 -0500

    set up app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name immiTrumpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the immiTrumpApp
+ */
+angular.module('immiTrumpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name immiTrumpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the immiTrumpApp
+ */
+angular.module('immiTrumpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
